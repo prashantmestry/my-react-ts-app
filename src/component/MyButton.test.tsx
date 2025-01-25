a9cbe8b109bb353692012328a0385e3fd9abb189
prashantmestry <mestry.prashant10@gmail.com>
2025-01-25 20:18:10 +0530

    coverage folder cached removed

--- src/component/MyButton.test.tsx
+++ src/component/MyButton.test.tsx
@@ -4,9 +4,9 @@ import MyButton from "./MyButton";
 
 test("Button renders with label and responds to click", () => {
   const handleClick = jest.fn();
-  render(<MyButton onClick={handleClick} label="Click Me" />);
+  render(<MyButton onClick={handleClick} label="Click Her" />);
 
-  const button = screen.getByText(/click me/i);
+  const button = screen.getByText(/click her/i);
   expect(button).toBeInTheDocument();
 
   fireEvent.click(button);
