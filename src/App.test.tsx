9ab649387ddd9da56273e957eba1c2be6545c02f
prashantmestry <mestry.prashant10@gmail.com>
2025-01-25 20:38:50 +0530

    title changed for test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 describe("test app", () => {
   it("title check", () => {
     render(<App />);
-    const title = screen.getByText(/Hello, React/i);
+    const title = screen.getByText(/Hello, React100/i);
     expect(title).toBeInTheDocument();
 
     let btn = screen.getByRole("button");
